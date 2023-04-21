8ad89e0f79d5cf94c05546240b0f5d284def0f1e
XionWCFM <101111364+XionWCFM@users.noreply.github.com>
2023-04-21 17:06:43 +0900

    can use tailwind

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # viteTailwind
+
 tailwind set up
+
+###tailwindcss를 사용한 버전입니다.###
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className=" bg-red-300">it goes Work!!</div>
+    </div>
+  );
+}
+
+export default App;
